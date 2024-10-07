d242833ee84acd5a02a231228733399782e07cda
chrispined <chrispineouda@gmail.com>
2024-10-07 13:07:55 +0300

    Added a Jenkinsfile with a Unit test

---
+++ Ci-Cd-30-day-challenge/Nodejs-Project/Day_One/test/test.js
@@ -0,0 +1,7 @@
+const assert = require('chai').assert;
+
+describe('App', () => {
+    it('should return Hello, World!', () => {
+        assert.equal('Hello, World!', 'Hello, World!');
+    });
+});
