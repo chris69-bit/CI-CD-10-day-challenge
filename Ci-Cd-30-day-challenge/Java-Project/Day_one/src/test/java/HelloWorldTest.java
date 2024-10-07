d02642fed3b9e8fde646a54486fb8cd85d0f4760
chrispined <chrispineouda@gmail.com>
2024-10-07 10:07:36 +0300

    Added a Jenkinsfile and test

---
+++ Ci-Cd-30-day-challenge/Java-Project/Day_one/src/test/java/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package main.java;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class HelloWorldTest {
+    @Test
+    public void testMain() {
+        assertEquals("Hello, World!", "Hello, World!");
+    }
+}
