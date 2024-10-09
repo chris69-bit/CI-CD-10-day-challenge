0a703184e4d1e0f5b5a281b62b26e1f94422bdf0
chrispined <chrispineouda@gmail.com>
2024-10-09 11:39:38 +0300

    Added a Docker build and a dockerfile

---
+++ Ci-Cd-30-day-challenge/Go-Project/Day_Three/main_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "testing"
+)
+
+func TestHelloWithArgument(t *testing.T) {
+    expected := "Hello, Jenkins!"
+    got := "Hello, Jenkins!"
+    if got != expected {
+        t.Errorf("Expected %s, but got %s", expected, got)
+    }
+}
+
